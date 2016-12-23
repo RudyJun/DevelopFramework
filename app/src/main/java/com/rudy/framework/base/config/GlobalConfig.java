e3b5e1fddadd60ea68fac75dbf6c3f3fec372b46
RudyJun <lzj_glk@163.com>
2016-12-23 11:00:55 +0800

    提交代码

---
+++ app/src/main/java/com/rudy/framework/base/config/GlobalConfig.java
@@ -0,0 +1,10 @@
+package com.rudy.framework.base.config;
+
+/**
+ * Created by RudyJun on 2016/12/23.
+ */
+
+public interface GlobalConfig {
+    //腾讯bugly APPKEY
+    String BUGLY_APPID = "ed977d066a";
+}
