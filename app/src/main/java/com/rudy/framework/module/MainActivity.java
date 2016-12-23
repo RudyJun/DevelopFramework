04b9760a82d2f07e464a3aca23aaa6eefbd511fa
RudyJun <lzj_glk@163.com>
2016-12-23 09:54:14 +0800

    更新代码

--- app/src/main/java/com/rudy/framework/module/MainActivity.java
+++ app/src/main/java/com/rudy/framework/module/MainActivity.java
@@ -1,13 +1,10 @@
 package com.rudy.framework.module;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.view.View;
-import android.widget.Button;
 
 import com.flyco.tablayout.CommonTabLayout;
 import com.flyco.tablayout.listener.CustomTabEntity;
@@ -16,10 +13,8 @@ import com.rudy.framework.R;
 import com.rudy.framework.base.entity.TabEntity;
 import com.rudy.framework.base.presenter.BasePresenter;
 import com.rudy.framework.base.view.BaseActivity;
-import com.rudy.framework.base.view.PreviewPictureActivity;
 import com.rudy.framework.module.contacts.view.ContactsFragment;
 import com.rudy.framework.module.discovery.view.DiscoveryFragment;
-import com.rudy.framework.module.index.view.ExampleActivity;
 import com.rudy.framework.module.index.view.IndexFragment;
 import com.rudy.framework.module.me.view.MeFragment;
 import com.rudy.framework.widget.ScrollViewPager;
@@ -34,12 +29,6 @@ import butterknife.BindView;
  */
 
 public class MainActivity extends BaseActivity {
-
-    @BindView(R.id.btScan)
-    Button btScan;
-
-    @BindView(R.id.btQuery)
-    Button btQuery;
 
     @BindView(R.id.mViewPager)
     ScrollViewPager mViewPager;
@@ -116,30 +105,6 @@ public class MainActivity extends BaseActivity {
             }
         });
 
-        btScan.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                List<String> pathList = new ArrayList<>();
-                pathList.add("http://rudy-test.oss-cn-shanghai.aliyuncs.com/android/42.jpg");
-                pathList.add("http://rudy-test.oss-cn-shanghai.aliyuncs.com/android/QQ%E6%88%AA%E5%9B%BE20161217144437.png");
-                pathList.add("http://pic23.nipic.com/20120830/668573_154825475126_2.jpg");
-                pathList.add("http://pic27.nipic.com/20130309/10401505_134039675000_2.jpg");
-                pathList.add("http://pic9.nipic.com/20100902/5615113_084913074019_2.jpg");
-                pathList.add("http://pic22.nipic.com/20120727/668573_145407326100_2.jpg");
-                pathList.add("http://k.zol-img.com.cn/dcbbs/24268/a24267112_01000.jpg");
-                pathList.add("http://pic.58pic.com/58pic/13/20/24/67k58PIC6st_1024.jpg");
-                PreviewPictureActivity.startActivity(MainActivity.this, pathList);
-
-            }
-        });
-
-        btQuery.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ExampleActivity.class);
-                startActivity(intent);
-            }
-        });
     }
 
 
--- app/src/main/java/com/rudy/framework/module/index/view/IndexFragment.java
+++ app/src/main/java/com/rudy/framework/module/index/view/IndexFragment.java
@@ -1,26 +1,69 @@
 package com.rudy.framework.module.index.view;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.rudy.framework.R;
 import com.rudy.framework.base.presenter.BasePresenter;
 import com.rudy.framework.base.view.BaseFragment;
+import com.rudy.framework.base.view.PreviewPictureActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by RudyJun on 2016/12/22.
  */
 
 public class IndexFragment extends BaseFragment {
-    private TextView tvIndex;
+
+    @BindView(R.id.tvIndex)
+    TextView tvIndex;
+
+    @BindView(R.id.btScan)
+    Button btScan;
+
+    @BindView(R.id.btQuery)
+    Button btQuery;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_index, null);
-        tvIndex = (TextView) view.findViewById(R.id.tvIndex);
+
+        ButterKnife.bind(this, view);
+
+        btScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                List<String> pathList = new ArrayList<>();
+                pathList.add("http://rudy-test.oss-cn-shanghai.aliyuncs.com/android/42.jpg");
+                pathList.add("http://rudy-test.oss-cn-shanghai.aliyuncs.com/android/QQ%E6%88%AA%E5%9B%BE20161217144437.png");
+                pathList.add("http://pic23.nipic.com/20120830/668573_154825475126_2.jpg");
+                pathList.add("http://pic27.nipic.com/20130309/10401505_134039675000_2.jpg");
+                pathList.add("http://pic9.nipic.com/20100902/5615113_084913074019_2.jpg");
+                pathList.add("http://pic22.nipic.com/20120727/668573_145407326100_2.jpg");
+                pathList.add("http://k.zol-img.com.cn/dcbbs/24268/a24267112_01000.jpg");
+                pathList.add("http://pic.58pic.com/58pic/13/20/24/67k58PIC6st_1024.jpg");
+                PreviewPictureActivity.startActivity(activity, pathList);
+
+            }
+        });
+
+        btQuery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(activity, ExampleActivity.class);
+                startActivity(intent);
+            }
+        });
         return view;
     }
 
